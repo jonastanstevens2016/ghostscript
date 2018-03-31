dae4d0828f78f617e0a49f4e6f3b86e9a4c475f6
Chushen Tan <jonastanstevens2016@gmail.com>
2018-03-31 14:40:04 -0400

    rectangle

--- main.cpp
+++ main.cpp
@@ -13,7 +13,19 @@ public:
 	}
 
 	void line (int x1, int y1, int x2, int y2) {
-		f << x1 << ' ' << y1 << " moveto " << x2 << ' ' << y2 << " lineto stroke\n";
+		f << 'newpath\n' << x1 << ' ' << y1 << " moveto " << x2 << ' ' << y2 << " lineto stroke\n";
+	}
+
+	void setColor(int r, int g, int b){
+		f << r << ' ' << g << ' ' << b << ' ' << "setrgbcolor\n";
+	}
+
+	void drawRect(int x, int y, int w, int h){
+		f << "newpath\n" << x << ' '<< y << " moveto " << x << ' ' << y + h << " lineto " << x + w << ' ' << y + h << " lineto " << x + w << ' ' << y << " lineto closepath stroke";
+	}
+
+	void fillRect(int x, int y, int w, int h){
+		f << "newpath\n" << x << ' '<< y << " moveto " << x << ' ' << y + h << " lineto " << x + w << ' ' << y + h << " lineto " << x + w << ' ' << y << " lineto closepath fill";
 	}
 };
 
@@ -21,8 +33,8 @@ int main() {
   Postscript p("test.ps");
   p.line(0,0, 300,400);  // 0 0 moveto 300 400 lineto stroke
   int r = 255, g = 0, b = 0;
-//  p.setColor(int r, int g, int b);
-//  p.drawRect(x, y, w, h);   // x y moveto x+w y lineto x+w y+h lineto ... closepath stroke
+  p.setColor(r, g, b);
+  p.drawRect(0, 0, 200, 200);   // x y moveto x+w y lineto x+w y+h lineto ... closepath stroke
 //  p.fillRect(x, y, w, h);   // x y moveto x+w y lineto x+w y+h lineto ... closepath fill
 //  p.setFillColor(int r, int g, int b); // 1.0 0.5 0 setrgbcolor
 //
